94e3dc6eb08328e4b3fa8b4019dffadc61aa9692
will <will@liao.com>
2020-05-21 21:02:43 +0800

    struct

--- src/com/oj/Solution.java
+++ src/com/oj/Solution.java
@@ -5,26 +5,7 @@ import java.util.*;
 
 class Solution {
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-        }
-    }
 
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
     //target = sum / k
     public boolean canPartitionKSubsets(int[] nums, int k) {
         int sum = 0;
